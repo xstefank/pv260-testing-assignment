ae8c5127a769f72cf587d4cd451326a580ac3932
xstefank <xstefank122@gmail.com>
2017-04-21 15:01:45 +0200

    add selenium test for task3

---
+++ task3/task3-selenium/src/test/java/cz/muni/fi/pv260/WikiHowSeleniumTest.java
@@ -0,0 +1,98 @@
+package cz.muni.fi.pv260;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:xstefank122@gmail.com">Martin Stefanko</a>
+ */
+public class WikiHowSeleniumTest {
+
+    private WebDriver webDriver;
+
+    @Before
+    public void before() {
+        webDriver = new FirefoxDriver();
+        webDriver.get("http://www.wikihow.com/");
+    }
+
+    @After
+    public void after() {
+        webDriver.quit();
+    }
+
+
+    @Test
+    public void testSeleniumSoftwareIsOnWikipedia() throws InterruptedException {
+
+        WebElement searchField = webDriver.findElement(By.name("search"));
+
+        searchField.sendKeys("google");
+
+        searchField.submit();
+
+        Thread.sleep(2000);
+
+        Assert.assertTrue("There is a \"How to Search Google\" wikiHow displayed",
+                webDriver.getPageSource().contains("//www.wikihow.com/Search-Google"));
+    }
+
+    @Test
+    public void testBrokenLinks() {
+        List<String> linkList = findAllOutboundLinks();
+
+
+        linkList.forEach(link -> {
+            try {
+                Assert.assertTrue(String.format("%s link is not valid", link),
+                        isLinkBroken(new URL(link)));
+            } catch (Exception e) {
+                Assert.fail(String.format("\"%s\" is not a valid URL: %s", link, e));
+            }
+        });
+    }
+
+    private List<String> findAllOutboundLinks() {
+
+        List<WebElement> elementList = webDriver.findElements(By.tagName("a"));
+
+        elementList.addAll(webDriver.findElements(By.tagName("img")));
+
+        List<String> hrefList = new ArrayList<>();
+
+        for (WebElement element : elementList) {
+            String hrefAttr = element.getAttribute("href");
+            if (hrefAttr != null && !hrefAttr.startsWith("#") && !hrefAttr.startsWith("/") && !hrefAttr.isEmpty()) {
+                hrefList.add(element.getAttribute("href"));
+            }
+        }
+
+        return hrefList;
+    }
+
+    public boolean isLinkBroken(URL url) throws Exception {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            connection.connect();
+            connection.getResponseMessage();
+            connection.disconnect();
+
+            return true;
+
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+}
